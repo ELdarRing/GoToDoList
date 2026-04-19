2cb61fd76d7910b7ba772ead8346574332d62f16
ELdarRing <eldarablyazov123@gmail.com>
2026-04-19 20:33:22 +0500

    com

--- main.go
+++ main.go
@@ -2,20 +2,29 @@ package main
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"net/http"
+	"os"
+	"strconv"
 )
 
 type Task struct {
 	ID   int    `json:"id"`
 	Text string `json:"text"`
+	Done bool   `json:"done"`
 }
 
 var tasks []Task
 var idCounter = 1
+const fileName = "tasks.json"
 
 func main() {
+	loadTasks()
+
 	http.HandleFunc("/", homeHandler)
 	http.HandleFunc("/tasks", tasksHandler)
+	http.HandleFunc("/delete", deleteHandler)
+	http.HandleFunc("/toggle", toggleHandler)
 
 	println("Server started at http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
@@ -39,7 +48,52 @@ func tasksHandler(w http.ResponseWriter, r *http.Request) {
 		idCounter++
 
 		tasks = append(tasks, task)
+		saveTasks()
 
 		json.NewEncoder(w).Encode(task)
+	}
+}
+
+func deleteHandler(w http.ResponseWriter, r *http.Request) {
+	id, _ := strconv.Atoi(r.URL.Query().Get("id"))
+
+	for i, t := range tasks {
+		if t.ID == id {
+			tasks = append(tasks[:i], tasks[i+1:]...)
+			break
+		}
+	}
+
+	saveTasks()
+}
+
+func toggleHandler(w http.ResponseWriter, r *http.Request) {
+	id, _ := strconv.Atoi(r.URL.Query().Get("id"))
+
+	for i := range tasks {
+		if tasks[i].ID == id {
+			tasks[i].Done = !tasks[i].Done
+			break
+		}
+	}
+
+	saveTasks()
+}
+
+func saveTasks() {
+	data, _ := json.Marshal(tasks)
+	ioutil.WriteFile(fileName, data, 0644)
+}
+
+func loadTasks() {
+	if _, err := os.Stat(fileName); err == nil {
+		data, _ := ioutil.ReadFile(fileName)
+		json.Unmarshal(data, &tasks)
+
+		for _, t := range tasks {
+			if t.ID >= idCounter {
+				idCounter = t.ID + 1
+			}
+		}
 	}
 }
